34ae066ad7234f8c76b6e137bf5ec52a49fc372c
James <jameskowal10@gmail.com>
2025-03-30 15:28:36 +0100

    Add custom tool

--- src/langchain_example/main.py
+++ src/langchain_example/main.py
@@ -1,21 +1,41 @@
+from typing import Any, Final
 from uuid import UUID
 from langchain_anthropic import ChatAnthropic
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessageChunk, HumanMessage
 from langchain_core.runnables import RunnableConfig
-from langchain_core.tools import BaseTool
+from langchain_core.tools import tool
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 
 from argparse import ArgumentParser
 
+from pydantic import BaseModel, Field
+
+MODEL_NAME: Final[str] = "claude-3-7-sonnet-20250219"
+
+
+class CalculatorInput(BaseModel):
+    a: int = Field(description="first number")
+    b: int = Field(description="second number")
+
+
+@tool("supermultiply", args_schema=CalculatorInput, return_direct=True)
+def supermultiply(a: int, b: int) -> float:
+    """Supermultiply two numbers."""
+    return (2 * a) / (b + 10)
+
 
 def multi_turn(
     model: ChatAnthropic, tools: list[TavilySearchResults], memory: MemorySaver
 ):
     agent_executor = create_react_agent(model, tools, checkpointer=memory)
-    config = RunnableConfig(run_id=UUID("2578368d-a567-4635-a470-1b75cd1c41ec"))
+    config = RunnableConfig(
+        configurable={"thread_id": "08c55a1c-eddd-40e6-9979-593e0326ad7b"}
+    )
+    step: AIMessageChunk
+    metadata: dict[Any, Any]
     for step, metadata in agent_executor.stream(
         {"messages": [HumanMessage(content="hi im bob!")]},
         stream_mode="messages",
@@ -29,6 +49,15 @@ def multi_turn(
     ):
         print(chunk)
         print("----")
+
+
+def custom_tool(model: ChatAnthropic, tools: list[Any]):
+    agent_executor = create_react_agent(model, tools)
+    for chunk in agent_executor.stream(
+        {"messages": [HumanMessage(content="what is the supermultiply of 2 and 3??")]},
+        stream_mode="messages",
+    ):
+        print(chunk)
 
 
 def weather(model: ChatAnthropic, tools: list[TavilySearchResults]):
@@ -49,18 +78,19 @@ def main():
     parser.add_argument("-v", "--verbose", action="store_true", help="debug output")
     parser.add_argument("-w", "--weather", action="store_true", help="weather example")
     parser.add_argument("-m", "--memory", action="store_true", help="multi-turn")
+    parser.add_argument("-c", "--custom", action="store_true", help="custom tool")
     parsed_args = parser.parse_args()
     # INFO: https://python.langchain.com/api_reference/community/tools/langchain_community.tools.tavily_search.tool.TavilySearchResults.html
     search = TavilySearchResults(max_results=2)
     tools = [search]
     memory = MemorySaver()
-    model = ChatAnthropic(
-        model_name="claude-3-7-sonnet-20250219", timeout=10, stop=None
-    )
+    model = ChatAnthropic(model_name=MODEL_NAME, timeout=10, stop=None)
     if parsed_args.weather:
         weather(model, tools)
     if parsed_args.memory:
         multi_turn(model, tools, memory)
+    if parsed_args.custom:
+        custom_tool(model, tools + [supermultiply])
 
 
 if __name__ == "__main__":
